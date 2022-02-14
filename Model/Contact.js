bc0dbff41ee650776866f612fd872c3d48360bc2
avinash-dc <jesuavinash1@gmail.com>
2022-02-14 12:25:16 +0530

    studentAPP

---
+++ Model/Contact.js
@@ -0,0 +1,24 @@
+import { Schema, model } from "mongoose";
+
+const ContactSchema = new Schema({
+  firstname: {
+    required: true,
+    type: String,
+  },
+  lastname: {
+    required: true,
+    type: String,
+  },
+  email: {
+    required: true,
+    type: String,
+  },
+  phone: {
+    required: true,
+    type: Number
+  }
+}, { timestamps: true });
+
+let ContactModel = model("contact", ContactSchema);
+
+export default ContactModel;
